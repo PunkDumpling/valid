117bcd5d772f1028e4ea9f9b771f75f06a519f3e
PunkDumpling <143641620+PunkDumpling@users.noreply.github.com>
2024-12-10 04:52:05 +0300

    Update scriptV.js

--- scriptV.js
+++ scriptV.js
@@ -28,31 +28,3 @@ failureIcon[serial].style.opacity = "0";
 successIcon[serial].style.opacity = "1";
 }
 }
-let passwordValidation = (password) => {
-    let lowerCaseLetters = /[a-z]/g;
-    let upperCaseLetters = /[A-Z]/g;
-    let numbers = /[0-9]/g;
-    if(password.value.length < 8) {
-        return "Password must be at least 8 characters long";
-    } else if(!password.value.match(lowerCaseLetters)) {
-        return "Password must contain at least one lowercase letter";
-    } else if(!password.value.match(upperCaseLetters)) {
-        return "Password must contain at least one uppercase letter";
-    } else if(!password.value.match(numbers)) {
-        return "Password must contain at least one number";
-    } else {
-        return "";
-    }
-}
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let passwordMessage = passwordValidation(password);
-    if (passwordMessage !== "") {
-        engine(password, 2, passwordMessage);
-    } else {
-        engine(username, 0, "Username cannot be blank");
-        engine(email, 1, "Email cannot be blank");
-        engine(password, 2, "Password cannot be blank");
-    }
-});
